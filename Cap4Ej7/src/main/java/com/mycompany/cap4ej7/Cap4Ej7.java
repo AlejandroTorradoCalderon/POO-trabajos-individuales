8f6bbb6ef79c720114b03cbc479363c6ffd7fe92
AlejandroTorradoCalderon <112442070+AlejandroTorradoCalderon@users.noreply.github.com>
2022-10-02 19:56:36 -0500

    Add files via upload

---
+++ Cap4Ej7/src/main/java/com/mycompany/cap4ej7/Cap4Ej7.java
@@ -0,0 +1,10 @@
+
+package com.mycompany.cap4ej7;
+
+public class Cap4Ej7 {
+
+    public static void main(String[] args) {
+        ventana fr = new ventana();
+       fr.setVisible(true);
+    }
+}
